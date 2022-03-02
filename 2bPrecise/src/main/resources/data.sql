1692b5bf7b18f18963e92a502cf9a7848bca071f
yoramnag <48060702+yoramnag@users.noreply.github.com>
2022-03-02 15:47:41 +0200

    adding data.sql

---
+++ 2bPrecise/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into manager(id, first_name, last_name) values(10001,'Yoram','Nagavker');
+insert into manager(id, first_name, last_name) values(10002,'Alex','Fainberg');
+
+insert into employee(id, first_name, last_name,position,manager_id) values(20001,'Chen','Belker','QA',10001);
+insert into employee(id, first_name, last_name,position,manager_id) values(20002,'Alon','Hanukayev','software developer',10001);
