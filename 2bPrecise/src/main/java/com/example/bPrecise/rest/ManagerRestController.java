f3c823ca95e2e327a935c2cf32aa80ad07a54acf
yoramnag <48060702+yoramnag@users.noreply.github.com>
2022-03-06 11:05:17 +0200

    get manager by id
    add get manager by id to ManagerRestController and ManagerService

--- 2bPrecise/src/main/java/com/example/bPrecise/rest/ManagerRestController.java
+++ 2bPrecise/src/main/java/com/example/bPrecise/rest/ManagerRestController.java
@@ -1,11 +1,18 @@
 package com.example.bPrecise.rest;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 import com.example.bPrecise.entity.Manager;
 import com.example.bPrecise.service.ManagerService;
@@ -19,10 +26,17 @@ public class ManagerRestController {
 	
 	// expose "/manager" and return list of managers
 	@GetMapping("/manager")
-	public List<Manager> findAll() {
+	public List<Manager> retrieveAllManagers() {
 		return managerService.findAll();
 	}
 	
-	
+	@GetMapping("/manager/{id}")
+	public EntityModel<Manager> retrieveManager(@PathVariable int id) {
+		Optional<Manager> manager = managerService.findById(id);
+		EntityModel<Manager> resource = EntityModel.of(manager.get());
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllManagers());
+		resource.add(linkTo.withRel("all-managers"));
+		return resource;
+	}
 
 }
--- 2bPrecise/src/main/java/com/example/bPrecise/service/ManagerService.java
+++ 2bPrecise/src/main/java/com/example/bPrecise/service/ManagerService.java
@@ -1,12 +1,14 @@
 package com.example.bPrecise.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.bPrecise.dao.ManagerRepository;
 import com.example.bPrecise.entity.Manager;
+import com.example.bPrecise.exception.ManagerNotFoundException;
 
 @Service
 public class ManagerService {
@@ -19,6 +21,15 @@ public class ManagerService {
 		return managerRepository.findAll();
 	}
 	
-	
+	//return manager
+	public Optional<Manager> findById(int id) {
+		Optional<Manager> manager = managerRepository.findById(id);
+		
+		if(!manager.isPresent()) {
+			throw new ManagerNotFoundException("id - " + id);
+		}
+		return manager;
+		
+	}
 
 }
