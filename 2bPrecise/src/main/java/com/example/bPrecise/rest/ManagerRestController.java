bfa607087e8c95e0b8ed6e02fe28909c91936766
yoramnag <48060702+yoramnag@users.noreply.github.com>
2022-03-13 15:29:34 +0200

    adding update manager
    adding update manager

--- 2bPrecise/src/main/java/com/example/bPrecise/rest/ManagerRestController.java
+++ 2bPrecise/src/main/java/com/example/bPrecise/rest/ManagerRestController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 
 import com.example.bPrecise.entity.Manager;
 import com.example.bPrecise.service.ManagerService;
@@ -47,14 +49,20 @@ public class ManagerRestController {
 	}
 	
 	@PostMapping("/manager")
-	public ResponseEntity<Object> createUser(@Valid @RequestBody Manager manager) {
+	public ResponseEntity<Object> createManager(@Valid @RequestBody Manager manager) {
 		manager.setId(0);
 		Manager savedManager = managerService.save(manager);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedManager.getId())
 				.toUri();
 
 		return ResponseEntity.created(location).build();
-		
+	}
+	
+	@PutMapping("/manager")
+	public ResponseEntity<Object> updateManager(@Valid @RequestBody Manager manager) {
+		managerService.findById(manager.getId());
+		managerService.save(manager);
+		return ResponseEntity.ok().build();
 	}
 
 }
