e86e1cd2809a956993fe0e9a759ac0fc2b3bd188
yoramnag <48060702+yoramnag@users.noreply.github.com>
2022-03-20 14:19:29 +0200

    add get one employee

--- 2bPrecise/src/main/java/com/example/bPrecise/rest/EmployeeRestController.java
+++ 2bPrecise/src/main/java/com/example/bPrecise/rest/EmployeeRestController.java
@@ -1,13 +1,21 @@
 package com.example.bPrecise.rest;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bPrecise.entity.Employee;
+import com.example.bPrecise.entity.Manager;
 import com.example.bPrecise.service.EmployeeService;
 
 @RestController
@@ -21,6 +29,15 @@ public class EmployeeRestController {
 	@GetMapping("/employee")
 	public List<Employee> retrieveAllEmployees() {
 		return employeeService.findAll();
+	}
+	
+	@GetMapping("/employee/{id}")
+	public EntityModel<Employee> retrieveEmployee(@PathVariable int id) {
+		Optional<Employee> employee = employeeService.findById(id);
+		EntityModel<Employee> resource = EntityModel.of(employee.get());
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllEmployees());
+		resource.add(linkTo.withRel("all-employee"));
+		return resource;
 	}
 
 }
--- 2bPrecise/src/main/java/com/example/bPrecise/service/EmployeeService.java
+++ 2bPrecise/src/main/java/com/example/bPrecise/service/EmployeeService.java
@@ -1,11 +1,13 @@
 package com.example.bPrecise.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.bPrecise.dao.EmployeeRepository;
 import com.example.bPrecise.entity.Employee;
+import com.example.bPrecise.exception.EmployeeNotFoundException;
 
 @Service
 public class EmployeeService {
@@ -16,6 +18,16 @@ public class EmployeeService {
 	// Return all employees
 	public List<Employee> findAll(){
 		return EmployeeRepository.findAll();
+	}
+	
+	//Return employee
+	public Optional<Employee> findById(int id) {
+		Optional<Employee> employee = EmployeeRepository.findById(id);
+		
+		if(!employee.isPresent()) {
+			throw new EmployeeNotFoundException("id - " + id);
+		}
+		return employee;
 	}
 
 }
