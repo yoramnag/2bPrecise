a542905a3e66a20e1ce9f5139a0d7ad26f915ab0
Yoram Nagavker <48060702+yoramnag@users.noreply.github.com>
2022-06-13 16:26:40 +0300

    adding unit test for the DAO layer
    add new unit tests for managerRepository , employeeRepository , reportRepository and taskRepository

---
+++ 2bPrecise/src/test/java/com/example/bPrecise/data/EmployeeRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.example.bPrecise.data;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.example.bPrecise.dao.EmployeeRepository;
+import com.example.bPrecise.entity.Employee;
+
+@DataJpaTest
+public class EmployeeRepositoryTest {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Test
+	public void testFindAllManagers() {
+		List<Employee> employee = employeeRepository.findAll();
+		assertEquals(2,employee.size());
+	}
+	
+	@Test
+	public void testFindOne() {
+		Employee employee = employeeRepository.findById(20002).get();
+		assertEquals("Alon",employee.getFirstName());
+	}
+
+}
